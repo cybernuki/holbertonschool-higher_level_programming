b7709208502b84f28b52c2b1a369a510a5109cf9
jhonatan arenas <1164@holbertonschool.com>
2019-12-10 21:48:51 +0000

    Fix error

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    last_digit = 0
-    if number < 0:
-        last_digit = (abs(number) % 10) * -1
-    else:
-        last_digit = number % 10
+    last_digit = abs(number) % 10
     print(last_digit, end="")
     return last_digit
