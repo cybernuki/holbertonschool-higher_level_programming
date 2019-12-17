d4ae2bdf0b44380c3807cf87226533709c0726a8
Cybernuki <1164@holbertonschool.com>
2019-12-17 13:33:08 -0500

    using copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,11 +3,8 @@ def new_in_list(my_list, idx, element):
     if (not (my_list) or idx < 0 or idx >= len(my_list)):
         return my_list
 
-    # Memcopy
-    new_list = []
-    for el in my_list:
-        new_list.append(el)
-
+    # copy
+    new_list = my_list.copy()
     # Modify
     new_list[idx] = element
     return new_list
