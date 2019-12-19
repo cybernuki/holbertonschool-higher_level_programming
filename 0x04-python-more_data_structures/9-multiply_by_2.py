e98c6f33e6229bcafe839aaf4c6039039c5c25de
Cybernuki <1164@holbertonschool.com>
2019-12-19 14:55:51 -0500

    Fix challenge 9

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     if not a_dictionary:
-        return None
+        return dict()
     return { key:a_dictionary[key]*2 for index, key in enumerate(a_dictionary) }
