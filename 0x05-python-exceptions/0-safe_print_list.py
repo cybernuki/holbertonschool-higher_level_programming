0ea230129684c1a634e22a9d5b3cc2ef04c45d87
Cybernuki <1164@holbertonschool.com>
2020-01-08 15:21:58 -0500

    Fix problems

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
@@ -7,3 +8,7 @@ def safe_print_list(my_list=[], x=0):
             break
     print("")
     return (i + 1)
+
+
+if __name__ == "__main__":
+    print(end="")
