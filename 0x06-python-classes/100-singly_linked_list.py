0b311fd7d009a5118b9a48a3d9ec8f10b96595cf
Cybernuki <1164@holbertonschool.com>
2020-01-09 17:24:45 -0500

    fix pep8 sytle

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -4,8 +4,8 @@ class Node:
 
     def __init__(self, data, next_node=None):
         """Initialize a Node.
-        
-        arg: 
+
+        arg:
             data (int): is the information that the node will save
             next_node (Node or None): is a reference to the next node
         """
@@ -16,7 +16,7 @@ class Node:
     def data(self):
         """data Getter"""
         return self.__data
-    
+
     @data.setter
     def data(self, value):
         """data Setter"""
@@ -28,13 +28,14 @@ class Node:
     def next_node(self):
         """next_node Getter"""
         return self.__next_node
-    
+
     @next_node.setter
     def next_node(self, value):
         """next_node Setter"""
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
 
 class SinglyLinkedList:
     """ Single linked list class"""
@@ -45,7 +46,7 @@ class SinglyLinkedList:
 
     def sorted_insert(self, value):
         """Add a new node in a sorted way
-        
+
         arg:
             value (Int): is the new value to add
         """
