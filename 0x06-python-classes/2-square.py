6ac676ca52bc273c5cd997e2a2786ca39475cdff
Cybernuki <1164@holbertonschool.com>
2020-01-09 10:41:16 -0500

    Add challenge 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """ A Square class """
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
