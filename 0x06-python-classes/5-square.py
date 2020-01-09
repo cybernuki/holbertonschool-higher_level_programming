6d4ec75eccd6b6c6eeaf30fc0fc5ca6e6a7eef6f
Cybernuki <1164@holbertonschool.com>
2020-01-09 11:31:25 -0500

    Add challenge 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+class Square:
+    """ A Square class """
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the area of this square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """size getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """size setter"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def my_print(self):
+        """ prints a grid with #"""
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
