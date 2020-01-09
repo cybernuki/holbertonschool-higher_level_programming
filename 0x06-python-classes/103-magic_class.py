4f779bdd2cf7d8e4045d7d7b4aefcbc9731a4f6b
Cybernuki <1164@holbertonschool.com>
2020-01-09 17:16:19 -0500

    Add contract in init function

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -6,7 +6,7 @@ class MagicClass:
     """A Circle class."""
 
     def __init__(self, radius=0):
-
+        """Init class."""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
