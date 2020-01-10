2dfa8ad833949708bdacf916701bd35f24551ffb
Cybernuki <1164@holbertonschool.com>
2020-01-09 20:57:08 -0500

    Fix error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,7 +48,7 @@ class Square:
         if self.__size == 0:
             print("")
             return
-        [print("") in range(self.__position)]
+        [print("") for i  in range(self.__position[1])]
         for i in range(self.__size):
             [print(" ", end="") for j in range(self.__position[0])]
             [print("#", end="") for j in range(self.__size)]
