2c23a291820d42472e17aab5cbaa8b7cefb7303f
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:09:14 -0500

    Add Challenge #8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+# 8-rectagle.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""
+Defines a Rectangle class that inherits by Base_Geometry
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
