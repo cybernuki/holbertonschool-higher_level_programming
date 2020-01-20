9d3b7330305753baf634415a3ff3c9da793c8a3f
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:09:21 -0500

    Add Challenge #10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+# 10-square.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""
+Defines a Square class that inherits by Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A Class that represents a Square"""
+    def __init__(self, size):
+        """Constructor method
+        Args:
+            size (int): is the sizes of the square
+        Raise:
+            TypeError - if size is not an int
+            ValueError - if size is less or equals to zero
+        """
+        super().__init__(size, size)
+        self.__size = size
