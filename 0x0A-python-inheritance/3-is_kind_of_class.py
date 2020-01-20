b5463a3d16e880768b31e06e7984293e4cfa90d3
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:08:50 -0500

    Add Challenge #3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+# 3-is_kind_of_class.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""
+Defines a method that evaluates if a object is an instance
+of the specified class
+"""
+
+
 def is_kind_of_class(obj, a_class):
+    """Evaluate if obj is an instance or subclass instance of a_class"""
+    return isinstance(obj, a_class)
