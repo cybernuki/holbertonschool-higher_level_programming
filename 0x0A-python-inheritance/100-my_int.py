8305d85db7ef2fd48313d27ead4c5517d8c417c0
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:09:41 -0500

    Add Advance #0

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+# 100-my_int.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""
+Defines a MyInt class that inherits by Int
+But has == and != operators inverted
+"""
+
+
+class MyInt(int):
+    """Class that has == and != operators inverted
+        Also, it inheritance by int
+    """
+
+    def __eq__(self, other):
+        """ compares if this instances is != of other instance"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """ compares if this instances is == of other instance"""
+        return super().__eq__(other)
