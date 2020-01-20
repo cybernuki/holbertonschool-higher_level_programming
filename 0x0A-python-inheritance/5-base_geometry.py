231b220590ced86ba1649f817c5feb81abcef9c2
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:09:01 -0500

    Add Challenge #5

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+# 5-base_geometry.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""
+Defines a empty BaseGeometry class
+"""
+
+
+class BaseGeometry():
+    """A Class that represent a geometry form"""
+    pass
