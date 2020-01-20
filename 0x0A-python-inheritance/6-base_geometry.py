56ec72e7cc2dd1d63bae7d9f29be7aa5fbc8f0f6
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:09:05 -0500

    Add Challenge #6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+# 6-base_geometry.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""
+Defines a BaseGeometry class with unimplemented Area Method
+"""
+
+
+class BaseGeometry():
+    """A Class that represent a geometry form"""
+    def area(self):
+        """Returns the area of this Rectangle"""
+        raise Exception("area() is not implemented")
