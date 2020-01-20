3d92d3ab3a37da1ce2ccac77b45473774d6829c1
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:09:47 -0500

    Add Advance #1

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+# 101-add_attribute.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""Try to adds a new attribute to a class"""
+
+
+def add_attribute(obj, att, value):
+    """Try to add a new attribute to a class
+    Args:
+        obj (any): is the objet target
+        att (string): is the name of the attribute
+        value (any): is the value of the attribute
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, att, value)
