eba5a43ccc96d96a9ea18aa3be957c99d17e041f
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:08:46 -0500

    Add Challenge #2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+# 2-is_same_class.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""
+Defines a method that evaluates if a object is an instance
+of the specified class
+"""
+
+
 def is_same_class(obj, a_class):
+    """Evaluate if the given obj is an ''exactly''
+    instance of the given class
+    """
+    return type(obj) is a_class
