9f3a99b28c620bbfadfca27432dd991d40ecfe21
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:08:55 -0500

    Add Challenge #4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+# 4-inherits_from.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""
+Defines a method that evaluates if a object is an sub class instance
+of the specified class
+"""
+
+
 def inherits_from(obj, a_class):
+    """Evaluates if obj is a sub class instance of a_class"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
