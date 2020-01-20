f95b053e9f856c3f4a11d43789326a3182adbc1c
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:08:29 -0500

    Add Challenge 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+# 0-lookup.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""Defines an object attribute lookup function"""
+
+
 def lookup(obj):
+    """returns a list object with all atributes an methods
+        of the given object.
+    """
+    return dir(obj)
