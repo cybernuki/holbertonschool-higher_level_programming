3f564fffaab107c2f8b4669757054a5d59c463f9
Cybernuki <1164@holbertonschool.com>
2020-01-20 16:09:25 -0500

    Add Challenge #11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+# 11-square.py
+# Jhonatan Arenas <1164@holbertonschool.com>
+"""
+Defines a Square class that inherits by Rectangle
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A Class that represents a Square"""
+    def __init__(self, size):
+        """Constructor method
+        Args:
+            size (int): is the sizes of the square
+        """
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Returns a informal representation of Square Class"""
+        return "[{}] {}/{}".format("Square", self.__size, self.__size)
