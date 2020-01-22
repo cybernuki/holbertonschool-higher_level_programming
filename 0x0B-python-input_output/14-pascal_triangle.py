3ea59455792cadcb304d2765c31e8e10a7e71541
Cybernuki <1164@holbertonschool.com>
2020-01-21 19:14:03 -0500

    Challenge 14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""This module defines a function that calculates
+The pascal's triangle"""
+
+def pascal_triangle(n):
+    """Returns a matrix with the nth level of the
+    pascal's triangle"""
+    if type(n) is not int:
+        raise TypeError("n must be an integer")
+    if n <= 0:
+        return []
+
+    matrix = [[1]]
+    if n == 1:
+        return matrix
+    for i in range (1, n):
+        matrix.append([])
+        prev, next = -1 , 0
+        while next <= i:
+            if prev == -1 or next == i:
+                matrix[i].append(1)
+            else:
+                matrix[i].append(matrix[i - 1][prev] + matrix[i - 1][next])
+            prev += 1
+            next += 1
+    return matrix
