43e28520106d4a65224475899b0685d828d1879d
Cybernuki <1164@holbertonschool.com>
2020-03-02 13:59:21 -0500

    Challenge 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- This scripts prints the full descipion of the table first table from the database hbtn_0c_0
+-- in the MySQL server.
+-- It uses SHOW COLUMNS FROM table_name; that is equivalent to DESCIBE table_name;
+SHOW COLUMNS FROM table_name;
