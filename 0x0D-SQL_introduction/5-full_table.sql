fa0c6ab513da54360887e8f6c8a40ed89ab44c6c
Cybernuki <1164@holbertonschool.com>
2020-03-02 14:04:18 -0500

    fix table_name to first_table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- This scripts prints the full descipion of the table first table from the database hbtn_0c_0
 -- in the MySQL server.
--- It uses SHOW COLUMNS FROM table_name; that is equivalent to DESCIBE table_name;
-SHOW COLUMNS FROM table_name;
+-- It uses SHOW COLUMNS FROM first_table; that is equivalent to DESCIBE first_table;
+SHOW COLUMNS FROM first_table;
