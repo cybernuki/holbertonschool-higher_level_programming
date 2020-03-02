b967ba12cf0d335cdc2bd1cd866398f0128a3b3f
Cybernuki <1164@holbertonschool.com>
2020-03-02 13:56:13 -0500

    challenge 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- This script creates a table called first_table in the curent database in the MySQL server
+CREATE TABLE IF NOT EXISTS first_table(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
