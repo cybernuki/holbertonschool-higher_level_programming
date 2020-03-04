c4d12b1bf4ebf5b87c4f68c59c7481ec0802da5b
Cybernuki <1164@holbertonschool.com>
2020-03-03 22:48:21 -0500

    Advance 2

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,2 @@
+-- lists all shows from hbtn_0d_tvshows_rate by their rating.
+SELECT s.title, SUM(rate) rating FROM tv_show_ratings r INNER JOIN tv_shows s ON s.id = r.show_id GROUP BY show_id ORDER BY rating DESC;
