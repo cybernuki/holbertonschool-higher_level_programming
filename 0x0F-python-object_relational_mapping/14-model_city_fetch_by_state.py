78ca6aed5ad7d85b23aad09132b22435c2a23d8a
Cybernuki <1164@holbertonschool.com>
2020-03-28 23:09:38 +0000

    Challange 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -21,6 +21,6 @@ if __name__ == '__main__':
 
     for city, state in session\
                         .query(City, State)\
-                        .filter(City.state_id == state.id)\
+                        .filter(City.state_id == State.id)\
                         .order_by(City.id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
