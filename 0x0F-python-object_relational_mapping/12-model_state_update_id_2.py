122bd4ca865ee56226f00d5600609f1bdebf72b1
Cybernuki <1164@holbertonschool.com>
2020-03-28 21:55:39 +0000

    challenge 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# This script adds the State object “Louisiana” to the database hbtn_0e_6_usa
+# This script changes the name of a State object
+# from the database hbtn_0e_6_usa
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -18,7 +19,6 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name="Louisiana")
-    session.add(new_state)
+    state = Session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
     session.commit()
-    print(new_state.id)
