cbcf7c09efe5acd94c6aa184252e9247842db09d
Cybernuki <1164@holbertonschool.com>
2020-03-28 21:31:13 +0000

    challenge 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# This script prints the State object with the name passed as argument
-# from the database hbtn_0e_6_usa
+# This script find states that contains a using sqlalchem
 from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
