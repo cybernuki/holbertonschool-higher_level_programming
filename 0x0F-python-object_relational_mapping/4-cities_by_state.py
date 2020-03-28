a6a9699c74256b2c1e5c8891d3f84555ac4788e1
Cybernuki <1164@holbertonschool.com>
2020-03-28 11:58:05 +0000

    challenge 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#This script show all values from states table
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+    usr_name, usr_pass, db_name = argv[1], argv[2], argv[3]
+    db = MySQLdb.connect(host='localhost', user=usr_name, passwd=usr_pass, db=db_name)
+    cur = db.cursor()
+    cur.execute("SELECT c.id, c.name, s.name FROM cities as c\
+    INNER JOIN states as s ON c.state_id = s.id\
+    ORDER BY c.id")
+
+    [print(state) for state in cur.fetchall()]
