0f09b9064e245caea42af5153c5d50fc316632e1
Cybernuki <1164@holbertonschool.com>
2020-03-28 21:58:15 +0000

    challenge 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -19,6 +19,6 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = Session.query(State).filter_by(id=2).first()
+    state = session.query(State).filter_by(id=2).first()
     state.name = "New Mexico"
     session.commit()
