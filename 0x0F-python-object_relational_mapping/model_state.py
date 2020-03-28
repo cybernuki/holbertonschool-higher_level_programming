d90319bbaa7429867931015624dd3eeb9b1d479c
Cybernuki <1164@holbertonschool.com>
2020-03-28 21:06:02 +0000

    challenge 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # It defines a State model
 # Inherits from SQLAlchemy Base and links to the MySQL table states.
 
+""" Model State """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
