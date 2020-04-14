a8773d89836a1c0c767f64f626b240fee1d86212
cybernuki <1164@holbertonschool.com>
2020-04-14 21:22:20 +0000

    challenge 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This script takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter."""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        q = ""
+    else:
+        q = argv[1]
+
+    res = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+
+    try:
+        data = res.json()
+        if data:
+            print("[{}] {}".format(data["id"], data["name"]))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
