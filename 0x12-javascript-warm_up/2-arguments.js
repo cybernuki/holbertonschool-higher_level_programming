da2f189dcb7c3e4e919a5ddd864bcc948721e400
cybernuki <1164@holbertonschool.com>
2020-05-05 17:55:59 +0000

    fix output message

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 // Author: Cybernuki
 // This script interpretates the argv
 
-if (process.argv.length > 2) {
+if (process.argv.length === 2) {
   console.log('Argument found');
+} else if (process.argv.lenght > 2) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
