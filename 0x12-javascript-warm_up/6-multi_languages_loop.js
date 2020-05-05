555c8cda09ca05b267c776e02f848a32c803c600
cybernuki <1164@holbertonschool.com>
2020-05-05 18:04:24 +0000

    fix logic

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,9 +3,6 @@
 // This script prints 3 lines: (like 1-multi_languages.js)
 // but by using an array of string and a loop
 
-const lines = ['C is fun',
-  'Python is cool',
-  'Javascript is amazing'];
-lines.forEach(line => {
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
   console.log(line);
-});
+}
