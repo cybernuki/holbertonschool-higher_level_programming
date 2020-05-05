6f619653e05e994bba4a9e5126342be0f63fdbbb
cybernuki <1164@holbertonschool.com>
2020-05-05 17:34:50 +0000

    fix style

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,11 +2,11 @@
 // Author: Cybernuki
 // This script prints the addition of 2 integers
 
-function add(a, b) {
-        return (a + b)
+function add (a, b) {
+  return (a + b);
 }
 
-const arg_1 = parseInt(process.argv[2]);
-const arg_2 = parseInt(process.argv[3]);
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
 
-console.log(add(arg_1, arg_2));
+console.log(add(arg1, arg2));
