952552b4180132f11dfc6168e0abc047bc77e869
cybernuki <1164@holbertonschool.com>
2020-05-13 00:48:45 +0000

    challenge 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = `${process.argv[2]}`;
+
+request(url, (error, Response, body) => {
+  if (error) return;
+
+  const result = {};
+  JSON.parse(body).forEach((task) => {
+    const user = task.userId;
+    const done = task.completed;
+    if (!result[user]) result[user] = 0;
+    result[user] = (done) ? result[user] + 1 : result[user];
+  });
+  console.log(result);
+});
