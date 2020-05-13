c164d3e2d0b8821cf46b75a1057e3a54259579b4
cybernuki <1164@holbertonschool.com>
2020-05-13 00:21:50 +0000

    new try challenge 3

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,12 +4,12 @@ const url = `${process.argv[2]}`;
 const searched = 'https://swapi-api.hbtn.io/api/people/18/';
 
 request(url, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+  if (!error) {
     const films = JSON.parse(body).results;
-    let count = 0;
-    films.forEach((film) => {
-      if (film.characters.includes(searched)) count++;
-    });
+    let count = films.reduce((count, film) => {
+      const filter = film.characters.find((character) => character.endsWith('/18/'));
+      return (filter) ? count + 1: count;
+    }, 0);
     console.log(count);
   }
 });
