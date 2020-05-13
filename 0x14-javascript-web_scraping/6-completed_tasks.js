88ce49e9aa03978e4a82a5a20ce3ada8d915799c
cybernuki <1164@holbertonschool.com>
2020-05-13 00:53:22 +0000

    challenge 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,14 +4,14 @@ const request = require('request');
 const url = `${process.argv[2]}`;
 
 request(url, (error, Response, body) => {
-  if (error) return;
-
-  const result = {};
-  JSON.parse(body).forEach((task) => {
-    const user = task.userId;
-    const done = task.completed;
-    if (!result[user]) result[user] = 0;
-    result[user] = (done) ? result[user] + 1 : result[user];
-  });
-  console.log(result);
+  if (!error) {
+    const result = {};
+    JSON.parse(body).forEach((task) => {
+      const user = task.userId;
+      const done = task.completed;
+      if (!result[user]) result[user] = 0;
+      result[user] = (done) ? result[user] + 1 : result[user];
+    });
+    console.log(result);
+  }
 });
